f412d69ed9a8373087e6c2de2848219e3e6cf842
jackson pegg <jacksonpegg01@outlook.com>
2023-09-03 20:06:52 -0400

    fixup!

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,16 +1,64 @@
-use crate::shapes::Circle;
+use wasm_bindgen::prelude::wasm_bindgen;
 
 #[derive(Debug)]
 pub enum CanvasError {
     IndexError,
 }
 
+#[derive(Debug, Clone, Copy)]
+pub struct Pixel {
+    pub x: usize,
+    pub y: usize,
+}
+
+impl Pixel {
+    fn new(x: usize, y: usize) -> Self {
+        return Pixel { x, y };
+    }
+}
+
+#[wasm_bindgen]
 #[derive(Debug)]
 pub struct Canvas {
-    width: usize,
-    height: usize,
+    pub width: usize,
+    pub height: usize,
     pixels: Vec<u32>,
 }
+impl Canvas {
+    pub fn size(&self) -> usize {
+        return self.width * self.height;
+    }
+
+    pub fn get_index(&self, pixel: Pixel) -> Result<usize, CanvasError> {
+        let index = pixel.y * self.width + pixel.x;
+        if index <= self.pixels.len() {
+            return Ok(index);
+        }
+        Err(CanvasError::IndexError)
+    }
+
+    pub fn get_pixel(&self, pixel: Pixel) -> Result<&u32, CanvasError> {
+        let index = self.get_index(pixel)?;
+        Ok(self.pixels.get(index).unwrap())
+    }
+    pub fn get_pixel_mut(&mut self, pixel: Pixel) -> Result<&mut u32, CanvasError> {
+        let index = self.get_index(pixel)?;
+        Ok(self.pixels.get_mut(index).unwrap())
+    }
+    pub fn get_coordinate(&self, index: usize) -> Result<Pixel, CanvasError> {
+        if index <= self.pixels.len() {
+            let x = index % self.width;
+            let y = (index - x) / self.width;
+            return Ok(Pixel::new(x, y));
+        }
+        Err(CanvasError::IndexError)
+    }
+    pub fn set_pixel(&mut self, pixel: Pixel, color: Color) -> Result<(), CanvasError> {
+        *self.get_pixel_mut(pixel)? = color.as_u32();
+        Ok(())
+    }
+}
+#[wasm_bindgen]
 impl Canvas {
     pub fn new(width: usize, height: usize) -> Self {
         return Canvas {
@@ -19,46 +67,11 @@ impl Canvas {
             pixels: vec![0; width * height],
         };
     }
-    pub fn fill(&mut self, value: u32) {
-        self.pixels.fill(value);
+    pub fn get_pixels_ptr(&self) -> *const u32 {
+        return self.pixels.as_ptr();
     }
-    pub fn get_index(&self, x: usize, y: usize) -> Result<usize, CanvasError> {
-        let index = y * self.width + x;
-        if index <= self.pixels.len() {
-            return Ok(index);
-        }
-        Err(CanvasError::IndexError)
-    }
-    pub fn get_pixels(&self) -> &Vec<u32> {
-        return &self.pixels;
-    }
-    pub fn get_pixel(&self, x: usize, y: usize) -> Result<&u32, CanvasError> {
-        let index = self.get_index(x, y)?;
-        Ok(self.pixels.get(index).unwrap())
-    }
-    pub fn get_pixel_mut(&mut self, x: usize, y: usize) -> Result<&mut u32, CanvasError> {
-        let index = self.get_index(x, y)?;
-        Ok(self.pixels.get_mut(index).unwrap())
-    }
-    pub fn get_coordinate(&self, index: usize) -> Result<(usize, usize), CanvasError> {
-        if index <= self.pixels.len() {
-            let x = index % self.width;
-            let y = (index - x) / self.width;
-            return Ok((x, y));
-        }
-        Err(CanvasError::IndexError)
-    }
-    pub fn set_pixel(&mut self, x: usize, y: usize, color: u32) -> Result<(), CanvasError> {
-        *self.get_pixel_mut(x, y)? = color;
-        Ok(())
-    }
-    pub fn draw_circle(&mut self, x: usize, y: usize, circle: Circle) {
-        for (index, pixel) in self.pixels.iter().enumerate() {
-            let (x2, y2) = self.get_coordinate(index).unwrap();
-            if x * x + y * y <= circle.radius * circle.radius {
-                println!("x: {}, y: {}", x, y);
-            }
-        }
+    pub fn fill(&mut self, color: Color) {
+        self.pixels.fill(color.as_u32());
     }
 }
 
@@ -73,3 +86,23 @@ impl std::fmt::Display for Canvas {
         Ok(())
     }
 }
+
+#[wasm_bindgen]
+#[derive(Debug, Copy, Clone)]
+pub struct Color {
+    pub a: u8,
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+}
+
+#[wasm_bindgen]
+impl Color {
+    pub fn new(b: u8, g: u8, r: u8) -> Self {
+        return Color { r, g, b, a: 255 };
+    }
+    pub fn as_u32(&self) -> u32 {
+        ((self.r as u32) << 16) | ((self.g as u32) << 8) | (self.b as u32) | ((self.a as u32) << 24)
+    }
+}
+
--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use draw_tool::canvas::Canvas;
-use draw_tool::shapes::*;
-
-fn main() {
-    let mut canvas = Canvas::new(50, 40);
-    canvas.fill(0xFF00FFFF);
-    canvas.set_pixel(1, 1, 0xFF000000).unwrap();
-    canvas.draw_circle(25, 20, Circle::new(10, 0xFFFFFFFF));
-}
