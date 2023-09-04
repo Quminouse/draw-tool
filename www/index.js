6c85040ce0acc0da64b3e22002c66746f9902c56
jackson pegg <jacksonpegg01@outlook.com>
2023-09-03 21:03:10 -0400

    fixup!

--- www/index.js
+++ www/index.js
@@ -11,14 +11,15 @@ js_canvas.height = HEIGHT;
 const ctx = js_canvas.getContext("2d");
 const imageData = ctx.getImageData(0, 0, WIDTH, HEIGHT);
 
-const rust_canvas = Canvas.new(WIDTH, HEIGHT);
+{
+    const rust_canvas = Canvas.new(WIDTH, HEIGHT);
+    rust_canvas.fill(Color.new(0, 0, 0));
+    rust_canvas.draw_circle(WIDTH / 2, HEIGHT / 2, Circle.new(50, Color.new(255, 255, 255)));
 
-rust_canvas.fill(Color.new(0, 0, 0));
-rust_canvas.draw_circle(WIDTH / 2, HEIGHT / 2, Circle.new(50, Color.new(255, 255, 255)));
+    render(rust_canvas);
+}
 
-render();
-
-function render() {
-    imageData.data.set(new Uint8ClampedArray(memory.buffer, rust_canvas.get_pixels_ptr(), WIDTH * HEIGHT * 4));
+function render(canvas) {
+    imageData.data.set(new Uint8ClampedArray(memory.buffer, canvas.get_pixels_ptr(), WIDTH * HEIGHT * 4));
     ctx.putImageData(imageData, 0, 0);
 }
